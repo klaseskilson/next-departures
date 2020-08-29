3609f432cce8ca02b314418215ca84299e8eeae3
Klas Eskilson <klas.eskilson@gmail.com>
2020-08-29 13:25:56 +0200

    Skip fancy nested config

--- config/initializers/sl.rb
+++ config/initializers/sl.rb
@@ -1,6 +1,4 @@
 # frozen_string_literal: true
 
-Rails.application.config.tap do |c|
-  c.x.sl.places_api_key = ENV['SL_PLACES_API_KEY']
-  c.x.sl.realtime_api_key = ENV['SL_REALTIME_API_KEY']
-end
+Rails.application.config.sl_places_api_key = ENV['SL_PLACES_API_KEY']
+Rails.application.config.sl_realtime_api_key = ENV['SL_REALTIME_API_KEY']
